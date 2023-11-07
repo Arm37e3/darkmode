12c7423c40eea75144ada9597a605c1c4813e329
Arm37e3 <akbbna48@gmail.com>
2023-11-07 11:14:54 +0700

    update

---
+++ script.js
@@ -0,0 +1,5 @@
+
+//const Switchtoggle = React.querySelector('input[type="checkbox"]');
+const Switchtoggle = document.querySelector('#sw');
+const toggleicon = document.querySelector("#toggle-icon");
+const nav = document.querySelector('#nav');
