382f6bf7d2dbddfdad09eeccb4f70082cb06a029
Arm37e3 <akbbna48@gmail.com>
2023-11-07 12:48:14 +0700

    update

--- script.js
+++ script.js
@@ -3,3 +3,17 @@
 const Switchtoggle = document.querySelector('#sw');
 const toggleicon = document.querySelector("#toggle-icon");
 const nav = document.querySelector('#nav');
+const img1 = document.querySelector('#img1')
+const img2 = document.querySelector('#img2')
+const img3 = document.querySelector('#img3')
+
+
+function switchmode(e){
+    if(e.target.checked){
+            console.log("Night Mode")
+    }else {
+            console.log("Day Mode")
+    }
+}
+
+Switchtoggle.addEventListener('change', switchmode);
